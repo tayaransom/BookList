de1e8cb8198d2599fcbdfb31fbbd73ca50084d37
tayaransom <71993512+tayaransom@users.noreply.github.com>
2022-07-23 21:49:44 -0400

    Add files via upload

---
+++ Controller/BookControllers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Controllers
+{
+
+    [Route("api/Book")]
+    [ApiController]
+    public class BookControllers : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BookControllers(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = _db.Book.FirstOrDefault(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        
+        }
+
+    }
+}
